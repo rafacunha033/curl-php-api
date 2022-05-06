ea92a20dbd0cf14e0947224bb53d82b02ae4f7b1
rafacunha033 <rafacunha.prof033@gmail.com>
2022-05-06 09:02:21 -0300

    first commit

--- source/Config.php
+++ source/Config.php
@@ -1,7 +1,6 @@
 <?php
 
 
-define('MAPBOX_API_KEY', 'pk.eyJ1IjoicmFmYWVsMDMzIiwiYSI6ImNrcXdjczdxMjBuYmkydWwzdXB6NnR2azEifQ.0dAPfaGksiya3f-TgF8gBg');
 
 
 
---
+++ source/curl.php
@@ -0,0 +1,37 @@
+<?php
+
+class Curl 
+{
+    public $url;
+
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function put() 
+    {
+
+    }
+
+    public function patch()
+    {
+        
+    }
+
+    public function delete()
+    {
+
+    }
+}
+
